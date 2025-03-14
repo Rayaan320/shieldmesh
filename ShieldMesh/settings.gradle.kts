9b18ed85e08b7682ec15735bdd0fbbe361d83199
Rayaan320 <rayaanm958@gmail.com>
2025-03-14 15:19:49 +0300

    Add files via upload

---
+++ ShieldMesh/settings.gradle.kts
@@ -0,0 +1,21 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/libs.versions.toml"))
+        }
+    }
+}
+rootProject.name = "ShieldMesh"
+include(":app")
